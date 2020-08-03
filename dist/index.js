a946fdf96371f1e1e05ea60376105cd5b47023a2
Gal Amouyal <gal.amouyal@ibm.com>
2020-08-04 00:29:22 +0300

    feat-all-looks-good

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,9 @@
-import Villa from "./models/Villa";
-import { Buildings } from "./models/Buildings";
-const a = new Buildings({ area: 1, address: "sss", city: "sds" }, 5);
-const villa = new Villa({ area: 1, address: "sss", city: "sds" }, 5, 5);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Villa_1 = require("./models/Villa");
+var Buildings_1 = require("./models/Buildings");
+var Office_1 = require("./models/Office");
+var building = new Buildings_1.Buildings({ area: 1, address: "sss", city: "sds" }, 5);
+var villa = new Villa_1.default({ area: 1, address: "sss", city: "sds" }, 5, 5);
+console.log(building, villa);
+console.log("TS COMPILE WORKS!!!", Office_1.username);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Villa from "./models/Villa";
-import { Buildings } from "./models/Buildings"
+import { Buildings } from "./models/Buildings";
 import { username } from "./models/Office";
-const a = new Buildings({ area: 1, address: "sss", city: "sds" }, 5)
+const building = new Buildings({ area: 1, address: "sss", city: "sds" }, 5);
 
 const villa = new Villa({ area: 1, address: "sss", city: "sds" }, 5, 5);
+console.log(building, villa);
+console.log("TS COMPILE WORKS!!!", username);
